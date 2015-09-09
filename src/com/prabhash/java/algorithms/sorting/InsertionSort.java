ba77e5e660f620f387bc145ff2c44f9250326095
Prabhash Rathore <prabhashrathore@gmail.com>
2015-09-09 06:41:41 -0700

    Reimplemented insertion sort algorithm

--- src/com/prabhash/java/algorithms/sorting/InsertionSort.java
+++ src/com/prabhash/java/algorithms/sorting/InsertionSort.java
@@ -13,39 +13,66 @@ public class InsertionSort {
 
 	private static final int[] list = {12, 4, 87, 1, 90, 21, 18, 34, 27, 19};
 	
-	public static void insertSort(int[] a) {
+	/**
+	 * Insertion Sort implementation.
+	 * 
+	 * In Insertion sort, there is no swapping done. In this algorithm, left side of array is always sorted and right hand side array is unsorted. We pick one element from right side
+	 * array and insert that in the correct position in left sorted array. This process involves continuously movinf left hand side array elements to one position right until the correct
+	 * position is determined for the element to be inserted.
+	 * 
+	 * T = O(n ^ 2)
+	 * S = O(1)
+	 * 
+	 * @param a
+	 * @throws Exception
+	 */
+	public static void insertionSort(int[] a) throws Exception {
 		
-		int size = a.length;
-		System.out.println("Size of given list is: " + size);
+		if(a == null) {
+			throw new Exception("Array is null");
+		}
 		
-		for(int i = 1; i < size; i++) {
-			int j = i;
-			while((j > 0) && (a[j] < a[j-1])) {
-				swap(a, j, j-1);
+		for(int i = 1; i < a.length; i++) {
+			
+			int current = a[i];
+			int j = i - 1;
+			
+			while(j >= 0 && a[j] > current) {
+				a[j+1] = a[j];
 				j--;
 			}
 			
+			a[j+1] = current;
+			
 		}
-		
-		System.out.println("Here is the sorted array: ");
-		for(int i = 0; i < size; i++)
-			System.out.println(a[i]);
 		
 	}
 	
-	private static void swap(int[] b, int c, int d) {
-		int temp = b[c];
-		b[c] = b[d];
-		b[d] = temp; 
+	private static void printArray() {
+		for(int i = 0; i < list.length; i++) {
+			System.out.print(list[i] + " ");
+		}
 	}
 	
 	public static void main(String[] args) {
 		
-		long t1 = System.currentTimeMillis();
-		insertSort(list);
-		long t2 = System.currentTimeMillis();
+		System.out.println("Original array:");
+		printArray();
 		
-		System.out.println("Time taken in insertion sort is: " + (t2 - t1));
+		long t1 = System.nanoTime();
+	
+		try {
+			insertionSort(list);
+		} catch(Exception e) {
+			System.out.println("\nArray is null");
+		}
+		
+		long t2 = System.nanoTime();
+		
+		System.out.println("\n\nTime taken in insertion sort is: " + (t2 - t1) + " nano seconds");
+		
+		System.out.println("\nSorted array:");
+		printArray();
 
 	}
 
