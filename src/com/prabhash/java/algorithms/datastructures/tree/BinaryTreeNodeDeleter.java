cc78a098c47adbecde170d6bf6da24293f13d76a
Prabhash Rathore <prabhashrathore@gmail.com>
2016-03-19 08:28:43 -0700

    Code update to delete any node in Binary Tree

--- src/com/prabhash/java/algorithms/datastructures/tree/BinaryTreeNodeDeleter.java
+++ src/com/prabhash/java/algorithms/datastructures/tree/BinaryTreeNodeDeleter.java
@@ -3,20 +3,16 @@ package com.prabhash.java.algorithms.datastructures.tree;
 public class BinaryTreeNodeDeleter {
 	
 	/**
-	 * Method 1:
-	 * 
 	 * To delete a node from Binary Tree, do following:
 	 * - Find the node to be deleted in the tree. If node not found then just return as node does not exist in tree.
-	 * - Find the right most node (leaf node) in the tree
-	 * - Replace node to be deleted with the right most leaf node
-	 * - Now delete the right most leaf node
-	 * 
-	 * This is not the best algorithm as instead of finding the right most node in the tree, we can replace node to be deleted with the
-	 * left most or right mode node from it's sub-tree. This will be more optimal solution. See method 2 below.
+	 * - If node to be deleted has two child nodes then find the rightmost leaf node, swap their data and delete right most leaf node. 
+	 * - If node to be deleted has only one child left or right then find the leaf node in their respective left or right sub-tree, swap
+	 * their values and delete that leaf node
+	 * - If node to be deleted is itself a leaf node then just delete that node.
 	 * 
 	 * @param root
 	 * @param data
-	 * @return
+	 * @return boolean
 	 */
 	public static boolean deleteNode(Node root, int data) {
 
@@ -25,7 +21,7 @@ public class BinaryTreeNodeDeleter {
 			return false;
 		}
 
-		Node nodeToBeDeleted = getNodeToBeDeleted(root, data);
+		Node nodeToBeDeleted = findNodeToBeDeleted(root, data);
 		
 		// Did not find a matching node
 		if(nodeToBeDeleted == null) {
@@ -36,74 +32,33 @@ public class BinaryTreeNodeDeleter {
 		if(nodeToBeDeleted.left == null && nodeToBeDeleted.right == null) {
 			return deleteLeafNode(root, nodeToBeDeleted);
 		}
-
-		Node deepestNode = findRightMostDeepNode(root);
-		nodeToBeDeleted.data = deepestNode.data;
+		
+		Node deepestNode = null;
+		if((nodeToBeDeleted.left != null && nodeToBeDeleted.right != null) || (nodeToBeDeleted.right != null)) {
+			
+			deepestNode = findRightMostDeepNode(nodeToBeDeleted); //find right most leaf node from the node to be deleted 
+			nodeToBeDeleted.data = deepestNode.data;
+		} else if(nodeToBeDeleted.left != null) {
+			
+			deepestNode = findLeftMostDeepNode(nodeToBeDeleted); // find left most leaf node from the node to be deleted
+			nodeToBeDeleted.data = deepestNode.data;
+		}
 
 		return deleteLeafNode(root, deepestNode);
-
-	}
-
-	/**
-	 * Method 2: Optimized
-	 * 
-	 * Here is how node is deleted from Binary Tree:
-	 * - Recurse through tree to find the node to be deleted. If node is not found then return false.
-	 * - If node to be deleted is found in tree then there are 4 possible options:
-	 * 		- If node is a leaf then set parent's left or right child to null whichever side this node is linked.
-	 * 		- [2 possible options] If node has only one sub-tree i.e. either left or right then just overwrite the node with root of left or
-	 * 			right child tree
-	 * 		- If node has both left and right child then find the right most leaf node on either left or right sub tree of this node
-	 * 			and overwrite node's data with right most leaf data and then  delete right most leaf node.  
-	 * 
-	 * Time Complexity: O(n)
-	 * 
-	 * @param root
-	 * @param data
-	 * @return boolean
-	 */
-	public static boolean deleteNodeOptimized(Node root, int data) {
-
-		if(root == null) {
-			return false;
-		}
-		
-		if(root.data == data) {
-
-			if(root.left != null && root.right != null) {
-
-				Node rightMostLeafNode = findRightMostDeepNode(root.right);
-				root.data = rightMostLeafNode.data;
-				return deleteLeafNode(root, rightMostLeafNode);
-			} else if(root.left != null) {
-				
-				Node leftMostLeafNode = findLeftMostDeepNode(root.left);
-				root.data = leftMostLeafNode.data;
-				return deleteLeafNode(root, leftMostLeafNode);
-			} else if(root.right != null) {
-				
-				Node rightMostLeafNode = findRightMostDeepNode(root.right);
-				root.data = rightMostLeafNode.data;
-				return deleteLeafNode(root, rightMostLeafNode);
-			} 
-		}
-		
-		return deleteNodeOptimized(root.left, data) || deleteNodeOptimized(root.right, data);
-	
 	}
 	
-	private static Node getNodeToBeDeleted(Node root, int data) {
+	private static Node findNodeToBeDeleted(Node root, int data) {
 
 		if(root == null) { // base condition
 			return null;
 		}
 
-		Node left = getNodeToBeDeleted(root.left, data);
+		Node left = findNodeToBeDeleted(root.left, data);
 		if(left != null) {
 			return left;
 		}
 
-		Node right = getNodeToBeDeleted(root.right, data);
+		Node right = findNodeToBeDeleted(root.right, data);
 		if(right != null) {
 			return right;
 		}
@@ -188,24 +143,18 @@ public class BinaryTreeNodeDeleter {
 		root.left = new Node(4);
 		root.left.left = new Node(1);
 		root.left.right = new Node(3);
+		// root.left.left.left = new Node(15);
 
 		root.right = new Node(6);
 		root.right.left = new Node(7);
 		root.right.right = new Node(9);
 
-		int nodeToBeDeleted = 1;
+		int nodeToBeDeleted = 8;
 		
 		if(deleteNode(root, nodeToBeDeleted)) {
 			System.out.println(nodeToBeDeleted + " is deleted");
 		} else {
 			System.out.println("Did not find " + nodeToBeDeleted + " in the tree");
 		}
-		
-//		if(deleteNodeOptimized(root, nodeToBeDeleted)) {
-//			System.out.println(nodeToBeDeleted + " is deleted");
-//		} else {
-//			System.out.println("Did not find " + nodeToBeDeleted + " in the tree");
-//		}
 	}
-
 }
