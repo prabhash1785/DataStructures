6b82241ba935d6974496f4108feb67f7546fa6f3
Prabhash Rathore <prabhashrathore@gmail.com>
2016-04-19 07:49:16 -0700

    Maximim Sum Sub-array problem

--- src/com/prabhash/java/algorithms/datastructures/array/MaxSumSubArray.java
+++ src/com/prabhash/java/algorithms/datastructures/array/MaxSumSubArray.java
@@ -1,26 +1,65 @@
 package com.prabhash.java.algorithms.datastructures.array;
 
+/**
+ * Reference: CLRS Maximum Sum-SubArray problem.
+ * 
+ * Application: Find best buy and sell stock price to make max profit.
+ * 
+ * @author prrathore
+ *
+ */
 public class MaxSumSubArray {
-
-	private int[] a = {12, 5, -3, 4, 6, -10, 7, -5, 9, 6, -8};
 	
 	/**
-	 * Solving this problem using Divide and Conquer technique
+	 * Solving this problem using Divide and Conquer technique.
 	 * 
-	 * Time complexity is O(n)
+	 * Max sum could lie in one of the possible 3 ways:
+	 * - Completely on left side of array
+	 * - Crossing the mid point
+	 * - Completely on right
+	 * 
+	 * So divide the problem in above mentioned 3 ways and then solve sub-problems.
+	 * 
+	 * Time complexity is O(n logn)
+	 * 
 	 * @author Prabhash Rathore
 	 */
-	public void findMaxSum() {
+	public static Output findMaxSumSubArray(int[] a, int low, int high) {
+		
+		if(high == low) {
+			return new Output(low, high, a[low]);
+		} else {
+			
+			int mid = (low + high) / 2;
+			
+			Output outputLeft = findMaxSumSubArray(a, low, mid);
+			Output outputRight = findMaxSumSubArray(a, mid + 1, high);
+			Output outputCrossingMidPoint = findMaxCrossingSubArray(a, low, mid, high);
+			
+			if(outputLeft.sum >= outputRight.sum && outputLeft.sum >= outputCrossingMidPoint.sum) {
+				return outputLeft;
+			} else if(outputRight.sum >= outputLeft.sum && outputRight.sum >= outputCrossingMidPoint.sum) {
+				return outputRight;
+			} else {
+				return outputCrossingMidPoint;
+			}
+		}
+	}
+	
+	/**
+	 * Find max sum assuming max sum crosses mid point in array. This is just one of the possible scenarios. Other
+	 * scenarios would be that max sum lies completely on left or completely on right.
+	 * 
+	 * Time Complexity: O(n)
+	 * 
+	 * @return
+	 */
+	private static Output findMaxCrossingSubArray(int[] a, int low, int mid, int high) {
+		
 		int leftMaxSum = -1;
 		int sum = 0;
-		
 		int maxLeftIndex = -1;
-				
-		int mid = a.length / 2;
-		System.out.println("\n\nOriginal size of array: " + a.length);
-		System.out.println("Here is the value of mid point: " + a[mid]);
-		
-		for(int i = mid; i >= 0; i--) {
+		for(int i = mid; i >= low; i--) {
 			sum += a[i];
 			if(sum > leftMaxSum)
 				leftMaxSum = sum;
@@ -29,10 +68,8 @@ public class MaxSumSubArray {
 		
 		int rightMaxSum = -1;
 		sum = 0;
-		
 		int maxRightIndex = -1;
-		
-		for(int i = mid+1; i < a.length; i++) {
+		for(int i = mid+1; i < high; i++) {
 			sum += a[i];
 			if(sum > rightMaxSum) {
 				rightMaxSum = sum;
@@ -42,22 +79,34 @@ public class MaxSumSubArray {
 		
 		int totalSum = leftMaxSum + rightMaxSum;
 		
-		System.out.println("Max value of sum is: " + totalSum);
-		System.out.println("Indices for this sum: i = " + maxLeftIndex + " and j = " + maxRightIndex);
+		return new Output(maxLeftIndex, maxRightIndex, totalSum);
+	}
+	
+	private static class Output {
 		
+		private int left;
+		private int right;
+		private int sum;
+		
+		public Output(int left, int right, int sum) {
+			this.left = left;
+			this.right = right;
+			this.sum = sum;
+		}
 	}
 	
 	public static void main(String[] args) {
 		
-		MaxSumSubArray maxSumSubArray = new MaxSumSubArray();
+		final int[] a = {12, 5, -3, 4, 6, -10, 7, -5, 9, 6, -8};
 		
 		System.out.println("Input Array is:");
-		for(int i : maxSumSubArray.a) {
+		for(int i : a) {
 			System.out.print(i + " ");
 		}
 		
-		maxSumSubArray.findMaxSum();
-
+		Output output = findMaxSumSubArray(a, 0, a.length - 1);
+		System.out.println("\nLeft index is: " + output.left);
+		System.out.println("Right index is: " + output.right);
+		System.out.println("Max sum is: " + output.sum);
 	}
-
 }
